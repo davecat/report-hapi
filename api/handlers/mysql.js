241cfa719cea61ac457d20fe0e7a88b8abb06373
organics <374564811@qq.com>
2017-09-28 02:52:44 +0800

    modify sql

--- api/handlers/mysql.js
+++ api/handlers/mysql.js
@@ -4,10 +4,10 @@
 var mysql= require('mysql');
 module.exports = function() {
   var connection = mysql.createConnection({
-    host: 'rm-2ze0v70uggy83t5ago.mysql.rds.aliyuncs.com',
+    host: 'rm-2ze92s92qz4d0jr18.mysql.rds.aliyuncs.com',
     user: 'lib',
     password: 'lib88888',
-    database: 'lib'
+    database: 'counter'
   });
 
   connection.connect();
